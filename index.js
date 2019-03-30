23d7239816f826f5c958c9089fece3c4a3e60487
simon3000 <simon3000@163.com>
2019-03-30 22:39:45 +0800

    record to txt

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
+const fs = require('fs')
 const got = require('got')
 const WebSocket = require('ws')
 
-const token = require('./token');
+const token = require('./token')
 
 ;
 (async () => {
-  let url = JSON.parse((await got('https://slack.com/api/rtm.connect?token=' + token)).body).url
-  // https://api.slack.com/methods/rtm.connect/
-  console.log(`URL: ${url}`)
-  const client = new WebSocket(url);
+  // https://api.slack.com/methods/conversations.list
+  let rawChannels = JSON.parse((await got(`https://slack.com/api/conversations.list?token=${token}`)).body).channels
+  let channels = {}
+  for (let i = 0; i < rawChannels.length; i++) {
+    channels[rawChannels[i].id] = rawChannels[i].name
+  }
 
+  // https://api.slack.com/methods/rtm.connect/
+  let url = JSON.parse((await got(`https://slack.com/api/rtm.connect?token=${token}`)).body).url
+  console.log(`URL: ${url}`)
+
+  const client = new WebSocket(url);
   client.on('open', () => {
     console.log('wss Connected')
   })
@@ -18,8 +26,12 @@ const token = require('./token');
     if (object.type == 'hello') {
       console.log('Hello from Slack')
     }
-    if (object.type == 'message') {
-      console.log(`${object.user}在${object.channel}说: ${object.text}`)
+    if (object.type == 'message' && object.text) {
+      let date = new Date()
+      let text = object.text
+      let channel = channels[object.channel] || object.channel
+      console.log(`${channel}: ${text}`)
+      fs.appendFileSync(`${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}-${channel}.txt`, text + '\n')
     }
   })
   client.on('close', () => {
