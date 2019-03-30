216e8fb73ed5fe68cb9aa0fb45a6b5d72c568f16
Recorder <example@example.com>
2019-03-30 23:51:59 +0800

    update

---
+++ update.sh
@@ -0,0 +1,2 @@
+GIT_AUTHOR_NAME="Recorder" GIT_AUTHOR_EMAIL="example@example.com" GIT_COMMITTER_NAME="Recorder" GIT_COMMITTER_EMAIL="example@example.com" git add .
+GIT_AUTHOR_NAME="Recorder" GIT_AUTHOR_EMAIL="example@example.com" GIT_COMMITTER_NAME="Recorder" GIT_COMMITTER_EMAIL="example@example.com" git -c commit.gpgsign=false commit -m "update"
